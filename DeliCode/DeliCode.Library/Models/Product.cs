4097df6b1d5774c6a4bdd03e5d0d08204429ee23
Jonatan Tran <jonatan.tran79@gmail.com>
2020-12-03 11:46:05 +0100

    Added Product Model

---
+++ DeliCode/DeliCode.Library/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliCode.Library.Models
+{
+    class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public string Url { get; set; }
+    }
+}
