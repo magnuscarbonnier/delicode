183f9b0cc40313c9fe98aae8a8514e640ab2e171
Magnus Carbonnier Becker <magnus.carbonnier@gmail.com>
2020-12-03 10:50:06 +0100

    Add ApplicationUser, add migration

---
+++ DeliCode/DeliCode.Web/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliCode.Web.Models
+
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
