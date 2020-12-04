0b9e3b1e8869ba4f4aa64255dd880438e8d939c5
Jonatan Tran <jonathan.t79@hotmail.com>
2020-12-04 14:14:28 +0100

    Added more API endpoints + UnitTests

--- DeliCode/DeliCode.ProductAPI.Tests/UnitTest1.cs
+++ DeliCode/DeliCode.ProductAPI.Tests/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace DeliCode.ProductAPI.Tests
         public void GetAllProductsShouldReturnListOfProducts()
         {
             ProductRepository repos = new ProductRepository();
-            var expected = repos.Products;
+            var expected = repos.products;
             List<Product> actual = repos.GetAllProducts();
             Assert.Equal(expected, actual);
         }
@@ -23,25 +23,34 @@ namespace DeliCode.ProductAPI.Tests
         public void GetProductShouldReturnSingleProduct()
         {
             ProductRepository repos = new ProductRepository();
-            var expected = repos.Products
+            var expected = repos.products
                 .Where(e => e.Id == new Guid("11223344-5566-7788-99AA-BBCCDDEEFF00"))
-                .SingleOrDefault(); ;
+                .SingleOrDefault();
             Product product = repos.GetProduct(new Guid("11223344-5566-7788-99AA-BBCCDDEEFF00"));
             Assert.Equal(expected, product);
         }
+        [Fact]
+        public void AddProductShouldAddNewProduct()
+        {
+            ProductRepository repos = new ProductRepository();
+            Product product = new Product { Id = Guid.NewGuid(), Name = "NyProduct", Description = "", Price = 150, ImageUrl = "#" };
+            int expected = repos.products.Count() + 1;
+            int actual = repos.AddProduct(product).Count();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void DeleteProductShouldDeleteProductFromList()
+        {
+            ProductRepository repos = new ProductRepository();
+            int expected = repos.products.Count() - 1;
+            int actual = repos.DeleteProduct(new Guid("11223344-5566-7788-99AA-BBCCDDEEFF00")).Count();
+            Assert.Equal(expected, actual);
+        }
+
         //Tests:
 
         // PUT: api/Products/5
         // PutProduct(Guid id, Product product)
-
-
-        // POST: api/Products
-        // PostProduct(Product product)
-
-
-        // DELETE: api/Products/5
-        // DeleteProduct(Guid id)
-
 
         // ProductExists(Guid id)
     }
