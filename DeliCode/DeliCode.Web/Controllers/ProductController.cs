ce812b2a8bb26fbc571a5f5f81c65b190186283d
mikael.tallbo <mikael.tallbo@gmail.com>
2020-12-04 09:07:24 +0100

    Product list
    Basic product index view with controller and dummy data

---
+++ DeliCode/DeliCode.Web/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliCode.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> test = new List<string>();
+            test.Add("Hej");
+            test.Add("Funkar");
+            test.Add("Detta");
+            test.Add("Verkligen");
+            return View(test);
+        }
+    }
+}
