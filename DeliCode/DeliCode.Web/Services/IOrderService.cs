6dbc2568b12b62939f27c29403ac640edcd69768
Magnus Carbonnier Becker <magnus.carbonnier@gmail.com>
2021-01-12 15:48:08 +0100

    added tests Orderservice

---
+++ DeliCode/DeliCode.Web/Services/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using DeliCode.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliCode.Web.Services
+{
+    public interface IOrderService
+    {
+        Task<Order> PlaceOrder(Order order);
+        Task<Order> GetOrderById(int? id);
+        Task<Order> UpdateOrder(int orderId, Order order);
+    }
+}
---
+++ DeliCode/DeliCode.Web/Services/OrderService.cs
@@ -0,0 +1,38 @@
+﻿using DeliCode.Web.Models;
+using DeliCode.Web.Repository;
+using DeliCode.Web.Services;
+using System.Threading.Tasks;
+
+namespace DeliCode.Web.Tests
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _repository;
+        public OrderService(IOrderRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Order> GetOrderById(int? id)
+        {
+            var order = await _repository.GetOrderById(id);
+            return order;
+        }
+
+        public async Task<Order> PlaceOrder(Order order)
+        {
+            order = await _repository.PlaceOrder(order);
+            return order;
+        }
+
+        public async Task<Order> UpdateOrder(int orderId, Order order)
+        {
+            if(orderId!=order.Id)
+            {
+                return null;
+            }
+            order = await _repository.UpdateOrder(order);
+            return order;
+        }
+    }
+}
