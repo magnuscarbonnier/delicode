676dc5d5570cc66d2ea90f7dd6e1a31816a83180
Magnus Carbonnier Becker <magnus.carbonnier@gmail.com>
2021-01-15 21:09:35 +0100

    orderstyling

--- DeliCode/DeliCode.Web/Models/Order.cs
+++ DeliCode/DeliCode.Web/Models/Order.cs
@@ -1,6 +1,7 @@
 ﻿using DeliCode.Library.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,19 +16,35 @@ namespace DeliCode.Web.Models
         public DateTime? BookedDeliveryDate { get; set; }
         public string UserId { get; set; }
         public string Email { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
+
         public string LastName { get; set; }
+        [Required]
+
         public string Address { get; set; }
+        [Required]
+
         public string ZipCode { get; set; }
+        [Required]
+
         public string City { get; set; }
+        [Required]
+
         public string Country { get; set; }
+        [Required]
+
         public string Phone { get; set; }
         public string ShippingNotes { get; set; }
         public bool IsTestOrder { get; set; }
+        public decimal ShippingPrice { get; set; }
         public decimal TotalPrice()
         {
-            return OrderProducts.Sum(x => x.Price * x.Quantity);
+            var sum=ShippingPrice+ OrderProducts.Sum(x => x.Price * x.Quantity);
+            return sum;
         }
+
         public List<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>();
     }
 }
